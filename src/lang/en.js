0b653b3d17adddd90bbba4bb614f7513bd62ba7b
ianb1204 <ian.bertin2004@gmail.com>
2025-06-20 14:17:48 +0200

    en.js

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,3 +1,72 @@
 export const en = {
-    'pokemon-team-planner' : "Pokemon team planner",
-}
+    "pokemon-team-planner": "Pokémon Team Builder",
+
+    "username": "Username:",
+    "password": "Password:",
+    "confirm-password": "Confirm your password:",
+    "no-account": "Don't have an account? Register",
+    "already-have": "Already have an account? Login",
+    "password-error": "The passwords entered do not match.",
+    "already-use": "Username is already taken.",
+    "login-error": "Incorrect username or password.",
+    "loading-data": "Loading data",
+    "search-pokemon": "Search for a Pokémon",
+
+    // POKEMONS
+
+    "abilities": "Abilities",
+    "ability": "Ability",
+    "egg-groups": "Egg groups",
+    "height": "Height",
+    "height-m": "{height} m",
+    "weight": "Weight",
+    "weight-kg": "{weight} kg",
+    "stats-title": "Stats",
+    "sensibilities": "Type effectiveness",
+
+    // Types
+
+    "fairy": "Fairy",
+    "fire": "Fire",
+    "water": "Water",
+    "steel": "Steel",
+    "ground": "Ground",
+    "normal": "Normal",
+    "fighting": "Fighting",
+    "flying": "Flying",
+    "poison": "Poison",
+    "electric": "Electric",
+    "grass": "Grass",
+    "psychic": "Psychic",
+    "ice": "Ice",
+    "bug": "Bug",
+    "rock": "Rock",
+    "ghost": "Ghost",
+    "dragon": "Dragon",
+    "dark": "Dark",
+
+    // Resistances
+
+    "r-0.25": "x1/4",
+    "r-0.5": "x1/2",
+    "r-1": "x1",
+    "r-2": "x2",
+    "r-4": "x4",
+    "r-0": "x0",
+
+    // TEAMS
+
+    "my-teams": "My Teams",
+    "create-team": "Team Creation",
+    "create-team-description": "Click the \"+\" button to add a Pokémon",
+    "team-empty": "You have no teams.",
+    "team-create": "Create a team",
+
+    "login": "Login",
+    "register": "Register",
+    "disconnect": "Logout",
+    "name": "Team name",
+    "validate": "Validate",
+    "save": "Save",
+    "cancel": "Cancel"
+};
